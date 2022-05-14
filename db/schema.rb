84f42ee71c7e9bc30a716ca196f73075620ff6b2
captproton <carl@wdwhub.net>
2022-05-14 10:31:02 -0700

    CHANGE: added subdomain to Site for customer site lookup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_205717) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_14_172836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -465,6 +465,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_205717) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subdomain"
     t.index ["team_id"], name: "index_sites_on_team_id"
   end
 
