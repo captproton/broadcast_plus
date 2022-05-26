a42b4e383a14766506fe61a1f5b5a49f4965067d
captproton <carl@wdwhub.net>
2022-05-26 15:12:19 -0700

    CHANGE: put time to read back into the /blog billboard

--- app/models/blog_article.rb
+++ app/models/blog_article.rb
@@ -1,4 +1,5 @@
 class BlogArticle < ApplicationRecord
+  WORDS_PER_MINUTE = 150
   # 🚅 add concerns above.
 
   belongs_to :blog_entry
@@ -18,6 +19,16 @@ class BlogArticle < ApplicationRecord
   # 🚅 add callbacks above.
 
   # 🚅 add delegations above.
+  def reading_time
+    # https://alexanderpaterson.com/posts/showing-estimated-reading-time-on-a-rails-blog-post
+    words_per_minute = 150
+    text =  self.body.to_plain_text
+    result = (text.scan(/\w+/).length / WORDS_PER_MINUTE).to_i ||= 1
+  end
+
+  def self.collect_pinned
+    where("pinned_value > ?", 0)
+  end
 
   # 🚅 add methods above.
 end
