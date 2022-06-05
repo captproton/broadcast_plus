30872ed101ae0e6646eac649a9a6301773cd4699
captproton <carl@wdwhub.net>
2022-06-04 19:35:50 -0700

    FIX: PublisherAccount not specific to site being rendered

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomeController < Public::CustomerSite::BaseController
   before_action :set_home_page
+  before_action :set_page_info
+  before_action :set_video_info
   layout "frontdoor"
 
   # GET /
@@ -16,4 +18,23 @@ class Public::HomeController < Public::CustomerSite::BaseController
     @frontpage_icons    = PublisherAccount.frontpage_icons
     @site_general_info  = @site.setting_general_infos.first
   end
+
+  def set_page_info
+    @page_info          = @site.setting_home_infos.first
+    @home_info          = @page_info
+    @frontpage_icons    = PublisherAccount.frontpage_icons    
+    @hero_image_url     = url_for(@page_info.hero_image)
+    @site_general_info  = @site.setting_general_infos.first
+  end
+
+  def set_video_info
+    @featured_youtube_video_url   = @page_info.featured_youtube_video_url
+    @youtube_video_id             = @featured_youtube_video_url.to_s.split('v=').last 
+    @featured_youtube_image_url   = "https://i.ytimg.com/vi/#{@youtube_video_id}/maxresdefault.jpg"
+    
+    if !@youtube_video_id.to_s.length == 11
+      @featured_youtube_image_url   = "https://via.placeholder.com/727x384.png?text=Featured+YouTube+Video"
+    end    
+  end
+
 end
